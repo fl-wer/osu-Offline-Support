0ebfb6e64fd4ef74e9ee97d8955fd78dbb1a10fc
fl-wer <flwer9650@gmail.com>
2022-03-15 01:44:53 +0000

    This file was missed somehow.

---
+++ Offline Support/BeatmapScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offline_Support
+{
+    public class BeatmapScore
+    {
+        // all of the variables of beatmap scores json
+        public string score_id, score, username, maxcombo, count50, count100, count300, countmiss,
+        countkatu, countgeki, accuracy, perfect, enabled_mods, user_id, date, rank, pp, replay_available;
+    }
+}
