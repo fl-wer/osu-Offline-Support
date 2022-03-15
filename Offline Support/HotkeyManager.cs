9626c6fcdd7f898852211e32c83a380afdeaa06a
fl-wer <flwer9650@gmail.com>
2022-03-15 01:35:20 +0000

    Code optimization.

--- Offline Support/HotkeyManager.cs
+++ Offline Support/HotkeyManager.cs
@@ -84,22 +84,6 @@ namespace Offline_Support
         private static int _id = 0;
     }
 
-    public class HotKeyEventArgs : EventArgs
-    {
-        public readonly Keys Key;
-        public readonly KeyModifiers Modifiers;
-
-        public HotKeyEventArgs(Keys key, KeyModifiers modifiers)
-        { Key = key; Modifiers = modifiers; }
-
-        public HotKeyEventArgs(IntPtr hotKeyParam)
-        {
-            uint param = (uint)hotKeyParam.ToInt64();
-            Key = (Keys)((param & 0xffff0000) >> 16);
-            Modifiers = (KeyModifiers)(param & 0x0000ffff);
-        }
-    }
-
     [Flags]
     public enum KeyModifiers { Alt = 1, Control = 2, Shift = 4, Windows = 8, NoRepeat = 0x4000 }
 }
